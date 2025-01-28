3947ed6882c8fc932fc3317c7d2a85226fcb15bf
Veniklive <danilvivivi@gmail.com>
2025-01-28 23:32:14 +0200

    Fix function algorithm, Add JS Doc, Add input validation

--- src/index.js
+++ src/index.js
@@ -1,22 +1,35 @@
+/** Calculates the winner's rating after a meeting with an opponent
+ * @param {number} winnerRate - The number of winner rating.
+ * @param {number} loserRate - The number of loser rating.
+ * @returns {(number|String)} The calculated value of new winner rating or a notification about incorrectly entered data.
+ */
+
 function calculateNewWinnerRate(winnerRate, loserRate) {
   let difference = winnerRate - loserRate;
   switch (true) {
     case winnerRate === 0:
       return loserRate;
-    case 0 >= difference >= -2:
+    case difference >= 0 && difference <= 2:
       return (winnerRate += 2);
-    case -2 > difference > -20:
-      return winnerRate++;
-    case -20 >= difference:
+    case difference > 2 && difference < 20:
+      return ++winnerRate;
+    case difference >= 20:
       return winnerRate;
-    case difference > 0:
-      return (loserRate - winnerRate + 5) / 3;
+    case difference < 0:
+      return (winnerRate += (loserRate - winnerRate + 5) / 3);
     default:
       return "Invalid input detected";
   }
 }
 
-let winnerRate = Number(prompt("Input winner rate: "));
-let loserRate = Number(prompt("Input loser rate: "));
+let winnerRate, loserRate;
 
-console.log(calculateNewWinnerRate(winnerRate, loserRate));
+do {
+  winnerRate = Number(prompt("Input winner rate: "));
+} while (isNaN(winnerRate) && winnerRate >= 0);
+
+do {
+  loserRate = Number(prompt("Input loser rate: "));
+} while (isNaN(loserRate) && loserRate >= 0);
+
+console.log(Number(calculateNewWinnerRate(winnerRate, loserRate).toFixed(1)));
