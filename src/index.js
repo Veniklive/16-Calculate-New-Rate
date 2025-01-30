765147a467c1f0879b291d560156ce1042831df5
Veniklive <danilvivivi@gmail.com>
2025-01-30 13:15:01 +0200

    Fix function logic

--- src/index.js
+++ src/index.js
@@ -1,35 +1,34 @@
 /** Calculates the winner's rating after a meeting with an opponent
  * @param {number} winnerRate - The number of winner rating.
  * @param {number} loserRate - The number of loser rating.
- * @returns {(number|String)} The calculated value of new winner rating or a notification about incorrectly entered data.
+ * @returns {number} The calculated value of new winner rating or a NaN.
  */
 
 function calculateNewWinnerRate(winnerRate, loserRate) {
   let difference = winnerRate - loserRate;
-  switch (true) {
-    case winnerRate === 0:
-      return loserRate;
-    case difference >= 0 && difference <= 2:
-      return (winnerRate += 2);
-    case difference > 2 && difference < 20:
-      return ++winnerRate;
-    case difference >= 20:
-      return winnerRate;
-    case difference < 0:
-      return (winnerRate += (loserRate - winnerRate + 5) / 3);
-    default:
-      return "Invalid input detected";
+  if (
+    !isNaN(winnerRate) &&
+    winnerRate >= 0 &&
+    !isNaN(loserRate) &&
+    loserRate >= 0
+  ) {
+    switch (true) {
+      case winnerRate === 0:
+        return loserRate;
+      case difference >= 0 && difference <= 2:
+        return (winnerRate += 2);
+      case difference > 2 && difference < 20:
+        return ++winnerRate;
+      case difference >= 20:
+        return winnerRate;
+      case difference < 0:
+        return (winnerRate += (loserRate - winnerRate + 5) / 3);
+    }
   }
+  return NaN;
 }
 
-let winnerRate, loserRate;
-
-do {
-  winnerRate = Number(prompt("Input winner rate: "));
-} while (isNaN(winnerRate) && winnerRate >= 0);
-
-do {
-  loserRate = Number(prompt("Input loser rate: "));
-} while (isNaN(loserRate) && loserRate >= 0);
+let winnerRate = 50;
+let loserRate = 52.123;
 
 console.log(Number(calculateNewWinnerRate(winnerRate, loserRate).toFixed(1)));
